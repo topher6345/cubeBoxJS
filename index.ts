325a20028f94b565ae8f6a449a62f7bf888ac2d1
Topher <topher6345@gmail.com>
2019-10-01 11:49:16 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,90 +3,99 @@ const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
 );
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 
-interface ALPHAS {
-  foo: number;
-  bar: number;
-}
-
-var alphas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
 type CubePosition = [number, number, number, number];
-type RGBA = [number, number, number, number];
 type RGB = [number, number, number];
 
-const Scriabin: Array<RGB> = [
-  [255, 0, 0],
-  [206, 154, 255],
-  [255, 255, 0],
-  [101, 101, 153],
-  [227, 251, 255],
-  [172, 28, 2],
-  [0, 204, 255],
-  [255, 101, 1],
-  [255, 0, 255],
-  [51, 204, 51],
-  [140, 138, 140],
-  [0, 0, 254]
-];
+class Cube {
+  ctx: CanvasRenderingContext2D;
+  position: CubePosition;
+  alpha: number;
+  colors: Array<RGB>;
+  note: number;
 
-type Cubes = [
-  CubePosition,
-  CubePosition,
-  CubePosition,
-  CubePosition,
-  CubePosition,
-  CubePosition,
-  CubePosition,
-  CubePosition
-];
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    position: CubePosition,
+    note: number
+  ) {
+    this.ctx = ctx;
+    this.position = position;
+    this.alpha = 0;
+    this.colors = [
+      [255, 0, 0],
+      [206, 154, 255],
+      [255, 255, 0],
+      [101, 101, 153],
+      [227, 251, 255],
+      [172, 28, 2],
+      [0, 204, 255],
+      [255, 101, 1],
+      [255, 0, 255],
+      [51, 204, 51],
+      [140, 138, 140],
+      [0, 0, 254]
+    ];
+    this.note = note;
+  }
 
-const cubeOrigin = [0, 0];
-const cubeSize = 50;
+  draw() {
+    this.alpha = this.alpha > 0 ? this.alpha - 1 : this.alpha;
 
-const cubes = [
-  [cubeOrigin[0] + 0, cubeOrigin[1] + 0, cubeSize, cubeSize],
-  [cubeOrigin[0] + 0, cubeOrigin[1] + 25, cubeSize, cubeSize],
-  [cubeOrigin[0] + 25, cubeOrigin[1] + 0, cubeSize, cubeSize],
-  [cubeOrigin[0] + 25, cubeOrigin[1] + 25, cubeSize, cubeSize],
-  [cubeOrigin[0] + 100, cubeOrigin[1] + 0, cubeSize, cubeSize],
-  [cubeOrigin[0] + 100, cubeOrigin[1] + 25, cubeSize, cubeSize],
-  [cubeOrigin[0] + 125, cubeOrigin[1] + 0, cubeSize, cubeSize],
-  [cubeOrigin[0] + 125, cubeOrigin[1] + 25, cubeSize, cubeSize]
-  // [cubeOrigin[0] + 300 + 0, 300 + cubeOrigin[1] + 25, cubeSize, cubeSize],
-  // [cubeOrigin[0] + 300 + 25, 300 + cubeOrigin[1] + 0, cubeSize, cubeSize],
-  // [cubeOrigin[0] + 300 + 25, 300 + cubeOrigin[1] + 25, cubeSize, cubeSize]
-];
+    const rgba = [...this.colors[this.note], this.alpha / 1000];
+    ctx.fillStyle = `rgba(${rgba.join(",")})`;
+    const [x, y, xw, yw] = this.position;
+    ctx.fillRect(x, y, xw, yw);
+  }
 
-function drawCube(
-  position: CubePosition,
-  rgba: RGBA,
-  ctx: CanvasRenderingContext2D
-) {
-  ctx.fillStyle = `rgba(${rgba.join(",")})`;
-  const [x, y, xw, yw] = position;
-  ctx.fillRect(x, y, xw, yw);
-}
-
-function draw(time: number, ctx: CanvasRenderingContext2D) {
-  for (let i = 0; i < 8; i++) {
-    let [r, g, b] = Scriabin[i];
-    let pos: CubePosition = <CubePosition>cubes[i];
-
-    alphas[i] = alphas[i] > 0 ? alphas[i] - 1 : alphas[i];
-    drawCube(pos, [r, g, b, alphas[i] / 1000], ctx);
+  activate() {
+    this.alpha = 1000;
+    this.note = Math.floor(Math.random() * 12);
   }
 }
-function main(time) {
+
+const cubeOrigin: [number, number] = [0, 0];
+const cubeSize: number = 50;
+
+const cubes: Cube[] = [
+  new Cube(ctx, [cubeOrigin[0] + 0, cubeOrigin[1] + 0, cubeSize, cubeSize], 0),
+  new Cube(ctx, [cubeOrigin[0] + 0, cubeOrigin[1] + 25, cubeSize, cubeSize], 1),
+  new Cube(ctx, [cubeOrigin[0] + 25, cubeOrigin[1] + 0, cubeSize, cubeSize], 2),
+  new Cube(
+    ctx,
+    [cubeOrigin[0] + 25, cubeOrigin[1] + 25, cubeSize, cubeSize],
+    3
+  ),
+  new Cube(
+    ctx,
+    [cubeOrigin[0] + 100, cubeOrigin[1] + 0, cubeSize, cubeSize],
+    4
+  ),
+  new Cube(
+    ctx,
+    [cubeOrigin[0] + 100, cubeOrigin[1] + 25, cubeSize, cubeSize],
+    5
+  ),
+  new Cube(
+    ctx,
+    [cubeOrigin[0] + 125, cubeOrigin[1] + 0, cubeSize, cubeSize],
+    6
+  ),
+  new Cube(
+    ctx,
+    [cubeOrigin[0] + 125, cubeOrigin[1] + 25, cubeSize, cubeSize],
+    7
+  )
+];
+
+function main(_time) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  draw(time, ctx);
+  cubes.forEach(cube => cube.draw());
   requestAnimationFrame(main);
   return;
 }
 
 for (let i = 0; i < 8; i++) {
-  document.getElementById(`foo-${i}`).onclick = e => {
-    alphas[i] = 700;
-  };
+  document.getElementById("foo-" + i).onclick = () => cubes[i].activate();
 }
 
 requestAnimationFrame(main);
