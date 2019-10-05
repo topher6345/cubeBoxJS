1ecbc057e8e12e496bc9c3510fd507607910106d
Christopher Saunders <topher6345@gmail.com>
2019-10-04 18:13:35 -0700

    Add urnJB

--- index.ts
+++ index.ts
@@ -431,3 +431,34 @@ function main() {
 }
 
 requestAnimationFrame(main);
+
+function* urnJB(length) {
+  let array = randArray(length);
+  let i = 0;
+  while (true) {
+    yield array[i];
+    i = i + 1;
+    if (i === length) {
+      array = randArray(length);
+      i = 0;
+    }
+  }
+}
+
+function randArray(length) {
+  let array = [];
+
+  for (let i = 0; i < length; i++) {
+    array[i] = i;
+  }
+
+  // Fisher-Yates shuffling algorithm
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+}
