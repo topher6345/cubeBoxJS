e3029939a5dc5fdc0bb05baadd51bbaf8de4752e
Christopher Saunders <topher6345@gmail.com>
2019-10-04 18:54:46 -0700

    Add chord gen

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
+type Degrees = [number, number, number, number, number, number, number];
+
+interface Scale {
+  Ionian: Degrees;
+  Lydian: Degrees;
+  Locrian: Degrees;
+  Phrygian: Degrees;
+  Aeolean: Degrees;
+  Dorian: Degrees;
+  Mixolydian: Degrees;
+}
+
+const Scales: Scale = {
+  Ionian: [0, 2, 4, 5, 7, 9, 11],
+  Lydian: [0, 2, 4, 6, 7, 9, 11],
+  Locrian: [0, 1, 3, 5, 6, 8, 10],
+  Phrygian: [0, 1, 3, 5, 7, 8, 10],
+  Aeolean: [0, 2, 3, 5, 7, 8, 10],
+  Dorian: [0, 2, 3, 5, 7, 9, 10],
+  Mixolydian: [0, 2, 4, 5, 7, 9, 10]
+};
+
 const canvas = <HTMLCanvasElement>document.getElementById("canvas");
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 
@@ -423,7 +445,21 @@ function changeVolume() {
   masterGainNode.gain.value = parseFloat(volumeControl.value);
 }
 
-function main() {
+let then = null;
+let chordSpeed = 2 * 1000;
+
+let voices = [urnJB(7), urnJB(7), urnJB(7), urnJB(7)];
+
+function main(now) {
+  if (!then) then = now;
+  if (now - then > chordSpeed) {
+    then = now;
+    voices.forEach(voice => {
+      debugger;
+      console.log(Scales["Ionian"][voice.next().value]);
+    });
+  }
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   cubes.forEach(cube => cube.draw());
   requestAnimationFrame(main);
