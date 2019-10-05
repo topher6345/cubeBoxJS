71f03926896f7999cd08c4d01115e9b2438e3247
Christopher Saunders <topher6345@gmail.com>
2019-10-05 12:25:47 -0700

    1570303547

---
+++ src/cube.ts
@@ -0,0 +1,49 @@
+type CubePosition = [number, number, number, number];
+type RGB = [number, number, number];
+
+export default class Cube {
+  ctx: CanvasRenderingContext2D;
+  position: CubePosition;
+  alpha: number;
+  colors: Array<RGB>;
+  note: number;
+
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    position: CubePosition,
+    note: number
+  ) {
+    this.ctx = ctx;
+    this.position = position;
+    this.alpha = 0;
+    this.colors = [
+      [255, 0, 0],
+      [206, 154, 255],
+      [255, 255, 0],
+      [101, 101, 153],
+      [227, 251, 255],
+      [172, 28, 2],
+      [0, 204, 255],
+      [255, 101, 1],
+      [255, 0, 255],
+      [51, 204, 51],
+      [140, 138, 140],
+      [0, 0, 254]
+    ];
+    this.note = note;
+  }
+
+  draw() {
+    this.alpha = this.alpha > 0 ? this.alpha - 1 : this.alpha;
+
+    const rgba = [...this.colors[this.note], this.alpha / 100];
+    this.ctx.fillStyle = `rgba(${rgba.join(",")})`;
+    const [x, y, xw, yw] = this.position;
+    this.ctx.fillRect(x, y, xw, yw);
+  }
+
+  play(degree: number) {
+    this.alpha = 70;
+    this.note = degree;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -7,61 +7,7 @@ import fetchBuffer from "./fetch-buffer";
 const CANVAS = <HTMLCanvasElement>document.getElementById("canvas");
 const CTX: CanvasRenderingContext2D = CANVAS.getContext("2d");
 
-type CubePosition = [number, number, number, number];
-type RGB = [number, number, number];
-
-class Cube {
-  ctx: CanvasRenderingContext2D;
-  position: CubePosition;
-  alpha: number;
-  colors: Array<RGB>;
-  note: number;
-
-  constructor(
-    ctx: CanvasRenderingContext2D,
-    position: CubePosition,
-    note: number
-  ) {
-    this.ctx = ctx;
-    this.position = position;
-    this.alpha = 0;
-    this.colors = [
-      [255, 0, 0],
-      [206, 154, 255],
-      [255, 255, 0],
-      [101, 101, 153],
-      [227, 251, 255],
-      [172, 28, 2],
-      [0, 204, 255],
-      [255, 101, 1],
-      [255, 0, 255],
-      [51, 204, 51],
-      [140, 138, 140],
-      [0, 0, 254]
-    ];
-    this.note = note;
-  }
-
-  draw() {
-    this.alpha = this.alpha > 0 ? this.alpha - 1 : this.alpha;
-
-    const rgba = [...this.colors[this.note], this.alpha / 100];
-    CTX.fillStyle = `rgba(${rgba.join(",")})`;
-    const [x, y, xw, yw] = this.position;
-    CTX.fillRect(x, y, xw, yw);
-  }
-
-  activate(note: string) {
-    this.alpha = 70;
-    this.note = noteArray.indexOf(note);
-  }
-
-  play(degree: number) {
-    this.alpha = 70;
-    this.note = degree;
-  }
-}
-
+import Cube from "./cube";
 const CUBE_ORIGIN: [number, number] = [33, 33];
 const CUBE_SIZE: number = 66;
 
