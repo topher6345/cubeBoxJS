57a86a75e2fa7bb218f4f286de7d1f2f1bfff291
Christopher Saunders <topher6345@gmail.com>
2019-12-11 18:21:29 -0800

    1576117289

--- src/index.tsx
+++ src/index.tsx
@@ -84,47 +84,107 @@ const onClick = (
   f: (this: HTMLElement, ev: MouseEvent) => any
 ) => s.addEventListener("click", f, false);
 
-const masterGain = sel("input[name='']");
+const masterGain = sel("input[name='masterGain']");
 onClick(masterGain, () => {});
-const etMasterFilterValue = sel("input[name='']");
-onClick(etMasterFilterValue, () => {});
-const asterControlState = sel("input[name='']");
-onClick(asterControlState, () => {});
-const setDecayTime = sel("input[name='']");
+const masterFilterValue = sel("input[name='masterFilterValue']");
+onClick(masterFilterValue, () => {});
+const masterControlState = sel("input[name='masterControlState']");
+onClick(masterControlState, () => {});
+const setDecayTime = sel("input[name='setDecayTime']");
 onClick(setDecayTime, () => {});
-const chordOctave = sel("input[name='']");
+const chordOctave = sel("input[name='chordOctave']");
 onClick(chordOctave, () => {});
-const setLfoFrequency = sel("input[name='']");
+const setLfoFrequency = sel("input[name='setLfoFrequency']");
 onClick(setLfoFrequency, () => {});
-const filterEnvelopeQ = sel("input[name='']");
+const filterEnvelopeQ = sel("input[name='filterEnvelopeQ']");
 onClick(filterEnvelopeQ, () => {});
-const detune = sel("input[name='']");
+const detune = sel("input[name='detune']");
 onClick(detune, () => {});
-const etFilterEnvelopeStartFrequency = sel("input[name='']");
+const etFilterEnvelopeStartFrequency = sel(
+  "input[name='etFilterEnvelopeStartFrequency']"
+);
 onClick(etFilterEnvelopeStartFrequency, () => {});
-const lfoWave = sel("input[name='']");
+const lfoWave = sel("input[name='lfoWave']");
 onClick(lfoWave, () => {});
-const amplitudeAttack = sel("input[name='']");
+const amplitudeAttack = sel("input[name='amplitudeAttack']");
 onClick(amplitudeAttack, () => {});
-const etFilterEnvelopeSustain = sel("input[name='']");
+const etFilterEnvelopeSustain = sel("input[name='etFilterEnvelopeSustain']");
 onClick(etFilterEnvelopeSustain, () => {});
-const oscialltorType = sel("input[name='']");
+const oscialltorType = sel("input[name='oscialltorType']");
 onClick(oscialltorType, () => {});
-const scale = sel("input[name='']");
+const scale = sel("input[name='scale']");
 onClick(scale, () => {});
-const setBlendMode = sel("input[name='']");
+const setBlendMode = sel("input[name='setBlendMode']");
 onClick(setBlendMode, () => {});
-const lfoAmount = sel("input[name='']");
+const lfoAmount = sel("input[name='lfoAmount']");
 onClick(lfoAmount, () => {});
-const mplitudeRelease = sel("input[name='']");
+const mplitudeRelease = sel("input[name='mplitudeRelease']");
 onClick(mplitudeRelease, () => {});
-const swipeFrequency = sel("input[name='']");
+const swipeFrequency = sel("input[name='swipeFrequency']");
 onClick(swipeFrequency, () => {});
-const swipeOctave = sel("input[name='']");
+const swipeOctave = sel("input[name='swipeOctave']");
 onClick(swipeOctave, () => {});
-const chordVelocity = sel("input[name='']");
+const chordVelocity = sel("input[name='chordVelocity']");
 onClick(chordVelocity, () => {});
-const swipeVelocity = sel("input[name='']");
+const swipeVelocity = sel("input[name='swipeVelocity']");
 onClick(swipeVelocity, () => {});
-const sustain = sel("input[name='']");
+const sustain = sel("input[name='sustain']");
 onClick(sustain, () => {});
+
+const masterGainHandler = (e: string) => hashStorage.update({ masterGain: e });
+const etMasterFilterValueHandler = (e: string) =>
+  hashStorage.update({ setMasterFilterValue: e });
+const asterControlStateHandler = (e: boolean) => {
+  hashStorage.update({ masterControlState: e });
+};
+const setDecayTimeHandler = (e: string) =>
+  hashStorage.update({ setDecayTime: e });
+const chordOctaveHandler = (e: string) =>
+  hashStorage.update({ chordOctave: parseInt(e) });
+const setLfoFrequencyHandler = (e: string) => {
+  hashStorage.update({ setLfoFrequency: e });
+};
+const filterEnvelopeQHandler = (e: number) => {
+  hashStorage.update({ filterEnvelopeQ: e });
+};
+const detuneHandler = (e: number) => {
+  hashStorage.update({ detune: e });
+};
+const FilterEnvelopeStartFrequencyHandler = (e: string) => {
+  hashStorage.update({ setFilterEnvelopeStartFrequency: e });
+};
+const lfoWaveHandler = (e: string) => {
+  hashStorage.update({ lfoWave: e });
+};
+const amplitudeAttackHandler = (e: string) => {
+  hashStorage.update({ amplitudeAttack: parseFloat(e) });
+};
+const FilterEnvelopeSustainHandler = (e: string) => {
+  hashStorage.update({ setFilterEnvelopeSustain: e });
+};
+const oscialltorTypeHandler = (e: string) => {
+  hashStorage.update({ oscialltorType: e });
+};
+const scaleHandler = (e: string) => {
+  hashStorage.update({ scale: e });
+};
+const setBlendModeHandler = (e: string) => {
+  hashStorage.update({ setBlendMode: e });
+};
+const lfoAmountHandler = (e: number) => {
+  hashStorage.update({ lfoAmount: e });
+};
+const mplitudeReleaseHandler = (e: string) => {
+  hashStorage.update({ amplitudeRelease: parseFloat(e) });
+};
+const swipeFrequencyHandler = (e: number) => {
+  hashStorage.update({ swipeFrequency: e });
+};
+const swipeOctaveHandler = (e: string) =>
+  hashStorage.update({ swipeOctave: parseInt(e) });
+const chordVelocityHandler = (e: number) =>
+  hashStorage.update({ chordVelocity: e });
+const swipeVelocityHandler = (e: number) => {
+  hashStorage.update({ swipeVelocity: e });
+};
+// const sustainHandler = (e: boolean) =>  hashStorage.update({ sustain: !e })
