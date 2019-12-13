6e957b3ea8bd83ae93124ed62a3f13c3afd7f20d
Christopher Saunders <topher6345@gmail.com>
2019-12-12 21:34:20 -0800

    1576215260

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.tsx",
+  entry: "./src/index.ts",
   devtool: "inline-source-map",
 
   devServer: {
